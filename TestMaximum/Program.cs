5e969dc5b0a272f38bee6f7eb9978c95eaafa54b
pbishal <pbishal60@gmail.com>
2021-05-03 21:24:21 +0530

    Extended the max method to take more than three parameter

--- TestMaximum/Program.cs
+++ TestMaximum/Program.cs
@@ -10,11 +10,21 @@ namespace TestMaximum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to test maximum problem");
-            //Console.WriteLine(MaximumValue.MaxIntValue(200,300,500));
-            Console.WriteLine(MaximumValue.MaxFloatValue(7.9f, 77.5f, 5.6f));
-            Console.WriteLine(MaximumValue.MaxStringValue("Apple", "Peach", "Banana"));
-            Console.ReadLine();
+            int[] Output = { 232, 323, 423, 523, 567 };
+            Generic<int> genericInt = new Generic<int>(Output);
+            genericInt.PrintValue();
+            Console.WriteLine();
+
+            double[] Output1 = { 10.5, 24.6, 7.6, 87.6, 7.7 };
+            Generic<double> genericdouble = new Generic<double>(Output1);
+            genericdouble.PrintValue();
+            Console.WriteLine();
+
+            //String array
+            string[] stringArray = { "789", "456", "852", "753", "159" };
+            Generic<string> genericString = new Generic<string>(stringArray);
+            genericString.PrintValue();
+            Console.Read();
         }
     }
 }
